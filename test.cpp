d6f3e69075b3c63282906c41d851c713f931499a
KG-korepetycje <glowicz10@gmail.com>
2023-07-23 20:39:26 +0200

    Some new functionality.

--- test.cpp
+++ test.cpp
@@ -13,7 +13,15 @@ void wyswietl_powitanie(string& imie, string& wiek)
 {
     int wiek_liczba = zmien_typ_wieku(wiek);
 
-    cout << "\n\nWitaj " << imie << "! Twoj wiek to: " << wiek_liczba << " lat.\n\n";
+    cout << "\nWitaj " << imie << "! Twoj wiek to: " << wiek_liczba << " lat.\n\n";
+}
+
+int silnia(int n)
+{
+    if (n==1)
+        return 1;
+    else
+        return n * silnia(n-1);
 }
 
 int main()
@@ -27,6 +35,11 @@ int main()
     cin >> wiek;
 
     wyswietl_powitanie(imie, wiek);
+
+    int n;
+    cout << "Podaj liczbe n do obliczenia n!: ";
+    cin >> n;
+    cout << n << "! = " << silnia(n) << endl;
 
     return 0;
 }
