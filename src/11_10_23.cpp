79925833d6be27f15dd59019d990ba30ccd07d1d
KG-korepetycje <glowicz10@gmail.com>
2023-10-11 20:48:53 +0200

    Lekcja z 11.10 - stos, nawiasowanie.

---
+++ src/11_10_23.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int n;
+string s;
+stack <int> stos;
+
+
+
+// bool czypoprawne(string s, int n)
+// {
+//     for(int i = 0; i<n; i++)
+//     {
+//         if (s[i]==')' && stos.empty())
+//             return false;
+//         if (s[i]=='(')
+//             stos.push('(');
+//         else
+//             stos.pop();
+//     }
+
+//     if (stos.empty())
+//         return true;
+//     else
+//         return false;
+
+// }
+
+bool czypoprawne_trudne(string s, int n)
+{
+    for(int i = 0; i<n; i++)
+    {
+        // Przypadek, gdy mamy za duzo nawiasow zamykajacych
+        if ((s[i]==')' || s[i]==']') && stos.empty())
+            return false;
+        
+        // Przypadek, gdy trafiamy na nawias otwierajacy okragly
+        if (s[i]=='(')
+            stos.push('(');
+        
+        // Przypadek, gdy trafiamy na nawias otwierajacy kwadratowy
+        if (s[i]=='[')
+            stos.push('[');
+
+        // Przypadek, gdy mamy nawias zamykajacy i cos jest na stosie
+        else
+        {
+            if ((stos.top()=='(' && s[i]==')') || (stos.top()=='[' && s[i]==']'))
+                stos.pop();
+        }
+    }
+
+    if (stos.empty())
+        return true;
+    else
+        return false;
+
+}
+
+
+int main()
+{
+
+    cin >> n;
+    cin >> s;
+
+    if (czypoprawne_trudne(s,n))
+        cout << "TAK";
+    else
+        cout << "NIE";
+
+    return 0;
+}
